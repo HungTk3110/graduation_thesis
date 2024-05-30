583645c172ea5aa6623b665a762963c46c3df8eb
hungtk <bazooka.hungtk@gmail.com>
2024-05-30 17:57:56 +0700

    add draw

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:untitled1/ui/home/components/nav_drawer.dart';
 import 'package:untitled1/ui/home/home_cubit.dart';
 import 'package:untitled1/ui/user_authentication/user_authentication_cubit.dart';
 
@@ -12,6 +16,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late HomeCubit cubit;
+  GlobalKey<ScaffoldState> _key = GlobalKey();
 
   @override
   void initState() {
@@ -23,7 +28,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffFAFAFA),
+      key: _key,
+      backgroundColor: Colors.white,
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: CupertinoButton(
+          onPressed: () {
+            _key.currentState?.openDrawer();
+          },
+          child: SizedBox(
+            width: 24.r,
+            height: 24.r,
+            child: SvgPicture.asset(
+              'assets/icon/menu.svg',
+            ),
+          ),
+        ),
+      ),
       body: _buildBodyWidget(),
     );
   }
